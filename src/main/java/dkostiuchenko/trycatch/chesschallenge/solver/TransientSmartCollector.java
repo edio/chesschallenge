d83e35a6c90dc5cc33e2b81276d548f9284f3ccd
Dmytro Kostiuchenko <edio@archlinux.us>
2015-08-13 15:08:35 +0200

    Printing solutions to stdout. After threshold, print only progress to stderr

--- src/main/java/dkostiuchenko/trycatch/chesschallenge/solver/TransientSmartCollector.java
+++ src/main/java/dkostiuchenko/trycatch/chesschallenge/solver/TransientSmartCollector.java
@@ -3,8 +3,8 @@ package dkostiuchenko.trycatch.chesschallenge.solver;
 import dkostiuchenko.trycatch.chesschallenge.chess.Board;
 
 /**
- * Doesn't store any solutions, prints them to stderr right on collecting. After first threshold prints every N'th
- * solution. After second threshold prints only number of solutions found.
+ * Doesn't store any solutions, prints them to stdout right on collecting. After first threshold prints every N'th
+ * solution. After second threshold prints only number of solutions found to stderr.
  */
 public class TransientSmartCollector implements SolutionCollector {
 
@@ -23,12 +23,14 @@ public class TransientSmartCollector implements SolutionCollector {
     public void collect(Board board) {
         totalSolutions++;
         if (totalSolutions <= firstThreshold) {
-            System.err.println(totalSolutions);
-            System.err.println(board);
+            System.out.println(totalSolutions);
+            System.out.println(board);
         } else if (totalSolutions % sparseFactor == 0) {
-            System.err.println(totalSolutions);
             if (totalSolutions <= secondThreshold) {
-                System.err.println(board);
+                System.out.println(totalSolutions);
+                System.out.println(board);
+            } else {
+                System.err.println(totalSolutions);
             }
         }
     }
